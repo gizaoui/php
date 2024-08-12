6ae4b98ab6547a37b70dfeaaf4d3e3930146f3d2
Gilles ZAOUI <gizaoui@gmail.com>
2024-08-12 18:43:28 +0200

    .

--- html/Blog/pages/single.php
+++ html/Blog/pages/single.php
@@ -7,15 +7,17 @@ $conn = pg_connect("host=mypostgres dbname=mydb user=postgres password=postgres"
 if ($conn) {
    echo 'Connection attempt <strong>succeeded</strong><br>';
 
-   $query = "INSERT INTO Article(titre, contenu, dat) VALUES ('Mon titre', 'Mon contenu', '".date("Y-m-d H:i:s")."');";
+   $query = "INSERT INTO Article(titre, contenu, dat) VALUES ('Mon titre', 'Mon contenu', '" . date("Y-m-d H:i:s") . "');";
    $result = pg_query($conn, $query);
    if (!$result) {
       echo "An error occurred<br>.";
       exit;
    } else {
-      echo "Query success at ". date("Y-m-d H:i:s");
-   }
+      echo "Query success at " . date("Y-m-d H:i:s") . "<hr>";
 
+      $result = pg_query($conn, "select * from Article");
+      var_dump(pg_fetch_all($result));
+   }
 } else {
    echo 'Connection attempt failed.<br>';
 }
