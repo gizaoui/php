329752313b49fd5f8d355812b11ff6072e5035da
Gilles ZAOUI <gizaoui@gmail.com>
2024-08-13 15:26:58 +0200

    .

--- html/Blog/pages/single.php
+++ html/Blog/pages/single.php
@@ -9,7 +9,7 @@ use App\Database;
 
 $db = new Database();
 $db->query("INSERT INTO Article(titre, contenu, dat) VALUES ('Mon titre', 'Mon contenu', '" . date("Y-m-d H:i:s") . "')");
-$db->query("SELECT * FROM Article");
+var_dump($db->query("SELECT * FROM Article"));
 
 echo 'Hello World !!';
 
