2fcf1111cf43ad8bd7a05683c4c7800b08d53b9f
Gilles ZAOUI <gizaoui@gmail.com>
2024-08-14 15:43:42 +0200

    .

--- html/Pattern/Singleton/config/Autoloader.php
+++ html/Pattern/Singleton/config/Autoloader.php
@@ -3,13 +3,13 @@
 namespace App;
 
 class Autoloader {
-
-   static function register() {
-      spl_autoload_register(array(__CLASS__, 'autoloader'));
-   }
-
-   static function autoloader($class_name) {
-      // var_dump($class_name);
-      require 'config/'.str_replace(__NAMESPACE__.'\\','',$class_name).'.php';
+    
+    static function register() {
+        spl_autoload_register ( array (__CLASS__,'autoloader' ) );
+    }
+    
+    static function autoloader($class_name) {
+        // print( $class_name."<br>");
+        require str_replace ( __NAMESPACE__ . '/', '', str_replace ( '\\', '/', $class_name ) ) . '.php';
     }
 }
