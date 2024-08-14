6b461f8b2d89a5585174764f7e5fb797336cbc90
Gilles ZAOUI <gizaoui@gmail.com>
2024-08-14 11:03:12 +0200

    .

--- html/Blog/app/Autoloader.php
+++ html/Blog/app/Autoloader.php
@@ -1,20 +1,18 @@
 <?php
+
 namespace App;
 
-class Autoloader
-{
-
-    static function register()
-    {
-        spl_autoload_register(array(
-            __CLASS__,
-            'autoloader'
-        ));
+class Autoloader {
+    
+    static function register() {
+        spl_autoload_register ( array (
+                __CLASS__,
+                'autoloader'
+        ) );
     }
-
-    static function autoloader($class_name)
-    {
+    
+    static function autoloader($class_name) {
         // print( $class_name."<br>");
-        require str_replace(__NAMESPACE__ . '/', '', str_replace('\\', '/', $class_name)) . '.php';
+        require str_replace ( __NAMESPACE__ . '/', '', str_replace ( '\\', '/', $class_name ) ) . '.php';
     }
 }
