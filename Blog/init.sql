3f7d32068bea3ec07e330ba9d21f4a2e6597409b
Gilles ZAOUI <gizaoui@gmail.com>
2024-10-06 20:42:55 +0200

    .

---
+++ Blog/init.sql
@@ -0,0 +1,12 @@
+
+CREATE TABLE Article (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(100),
+    content TEXT,
+    createAt TIMESTAMP
+);
+
+INSERT INTO Article(title, content, createAt) VALUES 
+('Premier article', 'Contenu du premier article', '2024-08-20 12:15:32'),
+('Second article', 'Contenu du second article', '2024-08-21 12:15:32'),
+('Troisième article', 'Contenu du troisième article', '2024-08-20 12:15:32');
